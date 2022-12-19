166294872025046a53abac6858c709053db86b4f
unknown <sohailshahidi1435@gmail.com>
2022-12-19 21:22:52 +0430

    Add new Mobile menu without Linters error

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-var menu = document.getElementById ("menu")
+const menu = document.getElementById('menu');
+// eslint-disable-next-line no-unused-vars
 function openmenu() {
   menu.style.display = 'block';
 }
+
+// eslint-disable-next-line no-unused-vars
 function closemenu() {
   menu.style.display = 'none';
 }
