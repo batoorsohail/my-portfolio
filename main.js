4e748f10d15ea983b2c9deda07ceb5d824a4dc9e
Dilsher Balouch <41000844+DilsherB@users.noreply.github.com>
2022-12-20 11:19:59 +0300

    Event Binding
    Binding the event to HTML

--- main.js
+++ main.js
@@ -7,3 +7,7 @@ function openmenu() {
 function closemenu() {
   menu.style.display = 'none';
 }
+
+//binding the event to HTML
+menu.addEventListener('click', openmenu);
+menu.addEventListener('click', closemenu);
