50de2b32d59078419973753f5fb9bec71350e385
unknown <sohailshahidi1435@gmail.com>
2023-01-02 14:54:32 +0430

    Add new popup window

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// menubar open and close
 const menu = document.getElementById('menu');
 
 function openmenu() {
@@ -8,10 +9,10 @@ function closemenu() {
   menu.style.display = 'none';
 }
 
-//  binding the event to HTML
 menu.addEventListener('click', openmenu);
 menu.addEventListener('click', closemenu);
 
+// work info data
 const workInfo = [
   {
     Id: '1',
@@ -34,7 +35,7 @@ const workInfo = [
   {
     Id: '3',
     name: 'Tonic',
-    img: 'Snapshoot Portfolio-2.png',
+    img: 'Snapshoot Portfolio-1.png',
     descriptions: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quae quos dolor numquam, quibusdam tempora illum nulla facere sunt deleniti?',
     technologies: ['HTMl', 'CSS', 'JavaScript'],
     liveServer: 'See Live',
@@ -43,7 +44,7 @@ const workInfo = [
   {
     Id: '4',
     name: 'Multi-Post Stories',
-    img: 'Snapshoot Portfolio-1.png',
+    img: 'Snapshoot Portfolio-2.png',
     descriptions: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quae quos dolor numquam, quibusdam tempora illum nulla facere sunt deleniti?',
     technologies: ['HTMl', 'CSS', 'JavaScript'],
     liveServer: 'See Live',
@@ -52,11 +53,70 @@ const workInfo = [
 
 ];
 
+// pop up tamplate
+const popupSection = document.getElementById('my-pop-up');
+
+function createPopup(project) {
+  const modal = document.createElement('div');
+  modal.innerHTML = `
+  <div class= "popup">
+      <div>
+        <i class="fa-solid fa-xmark" onclick="closePopup(); toggle()"></i>
+        <h3>${project.name}</h3>
+        <ul class="proj">
+          <li><a href="#">CANOPY</a></li>
+          <li><div class="dot"></div></li>
+          <li>Back End Dev</li>
+          <li><div class="dot"></div></li>
+          <li>2015</li>
+        </ul>
+        </div>
+        <img src="./images/${project.img}" alt="Work-sample" />
+        <div class="two-part">
+          <div class="left-popup">
+            <p class="pro">
+              ${project.descriptions}
+            </p>
+          </div>
+        <div class="right-popup">
+          <ul class="language">
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+          </ul>
+          <ul class="language lagu-desktop">
+            <li>Github</li>
+            <li>Ruby</li>
+            <li>BootStrap</li>
+          </ul>
+          <div class="thin-line"></div>
+            <button class="button popup-buttons">
+              <div>
+                ${project.liveServer} <i class="fa-solid fa-arrow-up-right-from-square"></i>
+              </div>
+            </button>
+            <button class="button popup-buttons">
+              <div>
+                ${project.sourceLink} <i class="fa-brands fa-square-github"></i></i>
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+  popupSection.appendChild(modal);
+}
+
+
+
+// work sample tamplate
 const workSample = document.querySelector('#portfolio');
+
 workInfo.forEach((project) => {
   const reverse = document.createElement('div');
   reverse.innerHTML = `
-   <div class = "works">
+   <div class= "works">
       <img src="./images/${project.img}" alt="Work-sample" class="img-transition"/>
       <div class="card-work work">
         <h3>${project.name}</h3>
@@ -75,68 +135,23 @@ workInfo.forEach((project) => {
           <li>CSS</li>
           <li>JavaScript</li>
         </ul>
-        <button class="button button-mg" onclick="openPopup(); toggle()">See Project</button>
+        <button class="button button-mg" onclick="openPopup()" toggle()">See Project</button>
       </div>
     </div>
-    <section class="popup-bg">
-      <div class="popup" id="popup">
-        <div>
-          <i class="fa-solid fa-xmark" onclick="closePopup(); toggle()"></i>
-          <h3>${project.name}</h3>
-          <ul class="proj">
-            <li><a href="#">CANOPY</a></li>
-            <li><div class="dot"></div></li>
-            <li>Back End Dev</li>
-            <li><div class="dot"></div></li>
-            <li>2015</li>
-          </ul>
-        </div>
-        <img src="./images/${project.img}" alt="Work-sample" />
-        <div class="two-part">
-          <div class="left-popup">
-            <p class="pro">
-              ${project.descriptions}
-            </p>
-          </div>
-          <div class="right-popup">
-            <ul class="language">
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>JavaScript</li>
-            </ul>
-            <ul class="language lagu-desktop">
-              <li>Github</li>
-              <li>Ruby</li>
-              <li>BootStrap</li>
-            </ul>
-            <div class="thin-line"></div>
-            <button class="button popup-buttons">
-              <div>
-                ${project.liveServer} <i class="fa-solid fa-arrow-up-right-from-square"></i>
-              </div>
-            </button>
-            <button class="button popup-buttons">
-              <div>
-                ${project.sourceLink} <i class="fa-brands fa-square-github"></i></i>
-              </div>
-            </button>
-          </div>
-        </div>
-      </div>
-    </section>
   `;
-  workSample.appendChild(reverse);
+  workSample.append(reverse);
 });
 
-const popup = document.getElementById('popup');
-
+// open and close pop up
 function openPopup() {
-  popup.classList.add('popup-open');
+  popupSection.classList.add('popup-open');
 }
 
 function closePopup() {
-  popup.classList.remove('popup-open');
+  popupSection.classList.remove('popup-open');
 }
+
+const popup = document.getElementById('popup');
 
 function toggle() {
   const blur = document.getElementById('blur');
@@ -148,3 +163,13 @@ function toggle() {
 popup.addEventListener('click', openPopup);
 popup.addEventListener('click', closePopup);
 popup.addEventListener('click', toggle);
+
+// formvalidation
+const email = document.querySelector('#email').value;
+const form = document.querySelector('#contact');
+form.addEventListener('submit', (event) => {
+  if (email !== email.toLowerCase()) {
+    event.preventDefault();
+    document.getElementById('validationMsg').innerHTML = 'Please use all small letters in Email';
+  }
+});
