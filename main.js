9f86e068740e80d0e42cd241d6121f35d6cef015
eyob <eyobiq1313@gmail.com>
2023-01-06 11:57:35 +0300

    Add modified local storage file

--- main.js
+++ main.js
@@ -206,3 +206,27 @@ form.addEventListener('submit', (event) => {
     error.innerHTML = 'Please add Email in lowercase!';
   }
 });
+
+const fields = document.querySelectorAll('.formData');
+const localData = {
+  fName: '',
+  lName: '',
+  email: '',
+  msg: '',
+};
+fields.forEach((field) => {
+  field.addEventListener('input', () => {
+    localData[field.name] = field.value;
+    localStorage.setItem('contactFormData', JSON.stringify(localData));
+  });
+});
+window.addEventListener('load', () => {
+  let retrievedData = {};
+  if (localStorage.getItem('contactFormData')) {
+    retrievedData = JSON.parse(localStorage.getItem('contactFormData'));
+    document.getElementById('fName').value = retrievedData.fName;
+    document.getElementById('lName').value = retrievedData.lName;
+    document.getElementById('email').value = retrievedData.email;
+    document.getElementById('msg').value = retrievedData.msg;
+  }
+});
