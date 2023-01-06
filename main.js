20d101afd8dc7ed7079d6179909aa825796dfc64
unknown <sohailshahidi1435@gmail.com>
2023-01-06 16:19:46 +0430

    Add update local storage

--- main.js
+++ main.js
@@ -207,26 +207,25 @@ form.addEventListener('submit', (event) => {
   }
 });
 
-const fields = document.querySelectorAll('.formData');
-const localData = {
-  fName: '',
-  lName: '',
+// ------------------------local storage---------------
+
+const localData = document.querySelectorAll('.form-input');
+const localStoreData = {
+  name: '',
   email: '',
-  msg: '',
+  message: '',
 };
-fields.forEach((field) => {
-  field.addEventListener('input', () => {
-    localData[field.name] = field.value;
-    localStorage.setItem('contactFormData', JSON.stringify(localData));
+localData.forEach((input) => {
+  input.addEventListener('input', () => {
+    localStoreData[input.name] = input.value;
+    localStoreData[input.email] = input.value;
+    localStoreData[input.message] = input.value;
+    localStorage.setItem('information', JSON.stringify(localStoreData));
   });
 });
-window.addEventListener('load', () => {
-  let retrievedData = {};
-  if (localStorage.getItem('contactFormData')) {
-    retrievedData = JSON.parse(localStorage.getItem('contactFormData'));
-    document.getElementById('fName').value = retrievedData.fName;
-    document.getElementById('lName').value = retrievedData.lName;
-    document.getElementById('email').value = retrievedData.email;
-    document.getElementById('msg').value = retrievedData.msg;
-  }
-});
+const informationStored = JSON.parse(localStorage.getItem('information'));
+if (informationStored) {
+  localData.forEach((element) => {
+    element.value = informationStored[element.name];
+  });
+}
